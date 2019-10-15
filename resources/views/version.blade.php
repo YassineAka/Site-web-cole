7786289515f1998f1af98358a5f385456baa9b3b
Yassine Choqi <45582@etu.he2b.be>
2019-10-15 12:33:23 +0200

    i have the capacity

--- resources/views/version.blade.php
+++ resources/views/version.blade.php
@@ -17,7 +17,7 @@
     <td style="padding:1em"> {{$version[2]}}</td>
     <td style="padding:1em"> {{$version[1]}}</td>
     <td style="padding:1em">{{$version[3]}}</td>
-    <td style="padding:1em">{{$version[4]}}</td>
+    <td style="padding:1em">{{$version[4]}} </td>
     <td style="padding:1em"><a href= https://git.esi-bru.be/PRJL-2019-2020/Projet-Attributions-Groupe-LesCerveaux/commit/{{$version[1]}}> Lien vers dernier commit </a></td>
 
 </tr>
