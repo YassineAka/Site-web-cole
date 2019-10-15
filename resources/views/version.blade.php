d191e519f5b3a2a4b4a67f422de157c4f56316b5
Yassine Choqi <45582@etu.he2b.be>
2019-10-15 11:44:55 +0200

    test du dernier commit.

--- resources/views/version.blade.php
+++ resources/views/version.blade.php
@@ -5,6 +5,6 @@
 <h1> Version du dernier commitageeee.</h1>
 
 {{$version[0]}}
-
+{{$version[1]}}
 <a href= https://git.esi-bru.be/PRJL-2019-2020/Projet-Attributions-Groupe-LesCerveaux/commit/{{$version[1]}}> Lien vers dernier commit </a>
 @endsection
