ddcf9ed4cf3d73b35e9df217ef0f5a5c2bfee906
Yassine Choqi <45582@etu.he2b.be>
2019-10-22 09:22:52 +0200

    Merge du master finish.

--- app/Http/Model/Model.php
+++ app/Http/Model/Model.php
@@ -11,12 +11,9 @@ class Model
 
     public static function getAllTeachers()
     {
-<<<<<<< HEAD
         //$pdo = new PDO("mysql:host=mysql-mohssine.alwaysdata.net;dbname=mohssine_test;charset=utf8", "mohssine", "catalan1070", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);        
-        $pdo = new PDO("mysql:host=mysql-lescerveaux.alwaysdata.net;dbname=lescerveaux_poc;charset=utf8", "191765", "Cerveaux123", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);        
-=======
+       // $pdo = new PDO("mysql:host=mysql-lescerveaux.alwaysdata.net;dbname=lescerveaux_poc;charset=utf8", "191765", "Cerveaux123", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);        
         $pdo = new PDO("mysql:host=localhost;dbname=test;charset=utf8", "root", "", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);        
->>>>>>> master
 
         $requetes = "SELECT teacher.id, teacher.name,teacher.firstName
                      From teacher ";        
