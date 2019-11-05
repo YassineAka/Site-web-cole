3a37097f830ce275a3d0fc773f01a977a8703aad
Kardillo Younes 45682 <g45682@PDG.ESI.HEB.SITE>
2019-11-05 17:48:02 +0100

    Ajout des formulaire caché

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -4,6 +4,13 @@
 
 <div class="row">
     <div class="col"style="margin-left: 2%;"> 
+        <div class="row">
+            <div class="col-9">
+            </div>
+            <div class="col">
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Ajouter un cours</button>
+            </div>
+        </div>
         <table class="table table-striped table-hover" id="MyTable">
             <h1>List Of Courses </h1>
             <thead>
@@ -22,13 +29,22 @@
                     <td class ="courses" id="{{$course->getId()}}">{{$course->getId()}}</td>
                     <td> {{$course->getTitle()}}</td>
                     <td> {{$course->getNbHours()}}</td>
-                    <td><button type="button" id="{{$course->getId()}}test" value="{{$course->getId()}}" class="btn btn-danger"> 🗑</button> <button type="button" class="btn btn-secondary">✎</button></td>
+                    <td><button type="button" id="{{$course->getId()}}test" value="{{$course->getId()}}" class="btn btn-danger del"> X</button> <button type="button" class="btn btn-secondary">✎</button></td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
     <div style="padding:2%;margin-right:5%;" >
+    
+        <div class="col collapse multi-collapse" id="multiCollapseExample2">
+            <div class="row">
+               <div class="col-10">
+               </div>
+               <div class="col">
+                  <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">X</button>
+                  </div>
+            </div> 
             <h1>Inscription</h1>
             <p>Veuillez entrer les cordonnées du cours à ajouter dans le formulaire ci-joint.</p>
             
@@ -69,7 +85,7 @@
 
     $(document).ready(function() {
         let sigle;
-        $(".btn").click(function() {
+        $(".del").click(function() {
         sigle = $(this).val();
         let url ="./courses/delete/"+sigle;
         $.get(url, function(jsData, status) {});
