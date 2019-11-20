45a803f2dd1d8738618856db84799d3bd8118151
Achetouan Mohammed <42933@etu.he2b.be>
2019-11-20 15:39:54 +0100

    Ajout du css pour navbar

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,10 +3,9 @@
 
 <head>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
  
   
   <title> @yield('title') </title>
@@ -42,6 +41,9 @@
         height: 100vh;
         margin: 0;
       }
+      .mb-4{
+        border-bottom: solid rgba(255, 255, 255,0.3);
+      }
 
     </style>
   </head>
@@ -49,6 +51,38 @@
 <body>
 
   <header>
+    <!--Navbar blue-->
+    <nav class="mb-4 navbar navbar-expand-lg navbar-dark unique-color-dark">
+        <a class="navbar-brand" href="#">ESI</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ url('') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="{{ url('teachers') }}">List Of Teachers</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="{{ url('courses') }}">List Of Courses</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="{{ url('missions') }}">List Of Missions</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="{{ url('groupes') }}">List Of Groups</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="{{ url('version') }}">Version</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+        <!--MDB Navbars
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
@@ -73,7 +107,7 @@
           </li>
         </ul>
       </div>
-    </nav>
+    </nav>-->
   </header>
   <h1> @yield('littletitle')</h1>
   <main>
