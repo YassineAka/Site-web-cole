4f1def1033b87a0f032ac6be2391d82efb8e0ffc
Kardillo Younes 45682 <g45682@PDG.ESI.HEB.SITE>
2019-11-26 15:05:20 +0100

    Ajout des boutons dans le home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,6 +30,7 @@
                             </div>
                         </div>
                     </a>
+                    
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <a href="{{ url('missions') }}" class="text-body">
